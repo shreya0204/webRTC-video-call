1350f99be4358bb67c2f00e854a7ed3f7399d98e
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2024-01-15 22:47:48 +0530

    setup socket in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css'
+import { SocketProvider } from './providers/Socket';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </SocketProvider>
     </div >
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSocket } from '../providers/Socket';
 
 const Home = () => {
+    const { socket } = useSocket();
+    const [email, setEmail] = useState();
+    const [roomId, setRoomId] = useState();
+
+    const handleJoinRoom = () => {
+        socket.emit("join-room", { roomId: roomId, emailId: email });
+    }
+
     return (
         <div className='home_container'>
             <div className='home_input__container'>
-                <input type="email" placeholder="Enter your email address" />
-                <input type="text" placeholder="Enter room code" />
-                <button className='home_enter_button'>Enter Room</button>
+                <input value={email} type="email" placeholder="Enter your email address" onChange={e => setEmail(e.target.value)} />
+                <input value={roomId} type="text" placeholder="Enter room code" onChange={e => setRoomId(e.target.value)} />
+                <button className='home_enter_button' onClick={handleJoinRoom}>Enter Room</button>
             </div>
         </div>
     );
