ead25bf538f3c86f92af1b4c816fb38f318a6edd
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2024-01-15 22:47:00 +0530

    fix: cors error

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Server } = require('socket.io');
 
-const io = new Server();
+const io = new Server({ cors: true });
 const app = express();
 
 app.use(bodyParser.json());
@@ -21,6 +21,6 @@ io.on('connection', (socket) => {
 });
 
 app.listen(8000, () => {
-    console.log('Server listening on port 3000');
+    console.log('Server listening on port 8000');
 });
 io.listen(8001);
