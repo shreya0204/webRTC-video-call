be29f1f0d1f3972f5e2787273799e6d3eea95bdf
Shreya Agarwal <www.shreyaagarwal.com@gmail.com>
2024-01-16 23:18:36 +0530

    provide peer & route to room

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css'
 import { SocketProvider } from './providers/Socket';
+import RoomPage from './pages/Room';
+import { PeerProvider } from './providers/Peers';
 
 function App() {
   return (
     <div>
       <SocketProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <PeerProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/:roomId" element={<RoomPage />} />
+          </Routes>
+        </PeerProvider>
       </SocketProvider>
     </div >
   );
